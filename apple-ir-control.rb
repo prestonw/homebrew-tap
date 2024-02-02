e085034209cd56b88beb7f9e0bca5ab36b0fc58b
prestonw <contact@webpango.com>
2024-02-02 22:56:09 +0000

    update requirements

--- apple-ir-control.rb
+++ apple-ir-control.rb
@@ -4,6 +4,8 @@ class AppleIrControl < Formula
   url "https://github.com/prestonw/apple-ir-control/archive/refs/tags/v0.9.9.tar.gz"
   sha256 "574a6e85e072b5fc56820c3ca750261c0dedc9d09792d4fd3541be4cee603559"
 
+  # Removed the depends_on :xcode => :build line
+
   def install
     system "make"
     bin.install "apple-ir-control"
@@ -11,15 +13,5 @@ class AppleIrControl < Formula
 
   test do
     system "#{bin}/apple-ir-control", "--version"
-  end
-
-  def caveats
-    <<~EOS
-      Command Line Tools for Xcode must be installed. You can install them with:
-      
-      xcode-select --install
-      
-      This is necessary for compilation.
-    EOS
   end
 end
