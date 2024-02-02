fda0841c1de1c7308bffe76183470689579f8c46
prestonw <contact@webpango.com>
2024-02-02 23:20:32 +0000

    Update apple-ir-control to v0.9.10

--- apple-ir-control.rb
+++ apple-ir-control.rb
@@ -2,7 +2,7 @@ class AppleIrControl < Formula
   desc "Tool to control the Apple IR receiver on macOS"
   homepage "https://github.com/prestonw/apple-ir-control"
   url "https://github.com/prestonw/apple-ir-control/archive/refs/tags/v0.9.9.tar.gz"
-  sha256 "574a6e85e072b5fc56820c3ca750261c0dedc9d09792d4fd3541be4cee603559"
+  sha256 "6e4ea963adcfa97ceb40a66d9a983f61ef081b686635e0b2519a13f495311482"
 
   # Removed the depends_on :xcode => :build line
 
